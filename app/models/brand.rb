1e88bc576c41194f95a208961bbda387c658e539
Erik Vogler <erikvogler@gmail.com>
2021-06-01 14:30:23 +0200

    switched back to nominatim

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,6 @@
 class Brand < ApplicationRecord
   has_many :brands_policies
   has_many :policies, through: brands_policies
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
