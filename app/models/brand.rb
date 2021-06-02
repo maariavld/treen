09acc800686bd1a8e7a4b3b320f91799c1db01d6
Maria Vlad <maaria.vld@gmail.com>
2021-06-02 18:51:33 +0200

    Home page carousel

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,5 @@
 class Brand < ApplicationRecord
-  has_many :brands_policies
-  has_many :policies, through: :brands_policies
+  has_many :brand_policies
+  has_many :policies, through: :brand_policies
   has_one_attached :photo
 end
