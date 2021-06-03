2115bb99ad1848ae05c16ade8ef8691da22aca61
Maria Vlad <maaria.vld@gmail.com>
2021-06-03 12:40:26 +0200

    map key

--- app/javascript/packs/init_mapbox.js
+++ app/javascript/packs/init_mapbox.js
@@ -12,7 +12,7 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     // mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
