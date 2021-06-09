42325582d10f2679ef092916989f6d776803e5e3
Cristina Oechsle <crisoechsle@gmail.com>
2021-06-09 18:29:01 +0200

    information popup improvement

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,9 +15,13 @@ class ReviewsController < ApplicationController
     @brand = Brand.find(params[:brand_id])
     @review.brand = @brand
     @review.user = current_user
-    @review.save
+    if @review.save
+      redirect_to brand_path(@brand)
+    else
+      redirect_to brand_path(@brand), notice: 'You need to specify a rating'
+      # render "shared/reviewform"
+    end
     authorize @review
-    redirect_to brand_path(@brand)
     # @review = Review.new(review_params)
     # @review.user = current_user
 
