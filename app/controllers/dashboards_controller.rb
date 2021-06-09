0f753807711d625cd364dfb5a529c472ceb31ce2
Maria Vlad <maaria.vld@gmail.com>
2021-06-09 18:30:41 +0200

    dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,19 @@
 class DashboardsController < ApplicationController
   before_action :skip_authorization
   before_action :set_user, only: [:show, :destroy]
+  before_action :set_brand, only: [:edit, :update]
   def show
    @brands = current_user.brands
   end
+ def edit
+    # @brand = Brand.find(params[:id])
+  end
 
+  def update
+    # @brand = Brand.find(params[:id])
+    @brand.update(brand_params)
+    redirect_to dashboards_path(current_user)
+  end
   def destroy
     @user.destroy
     respond_to do |format|
@@ -17,5 +26,13 @@ class DashboardsController < ApplicationController
 
   def set_user
     @user = current_user
+  end
+
+  def set_brand
+    @brand = Brand.find(params[:user_id])
+  end
+
+   def brand_params
+    params.permit(:status)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2021_06_07_154748) do
     t.bigint "brand_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
     t.index ["brand_id"], name: "index_reviews_on_brand_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
